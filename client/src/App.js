206821cccd6e66cf480af1cad9921f5494724b3e
humanSharma <humansharmaindia@gmail.com>
2023-02-11 18:22:07 +0530

    SIGNUP SIGNIN ADDITION IN CLIENT

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css';
+import SignUp from './pages/signup';
+import SignIn from './pages/signin';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" exact element={<SignUp />} />
+        <Route path="/signin" exact element={<SignIn />} />
+        {/* <Route path="/" exact element={<HomePage />} />
+        <Route path="auth/signin" element={<SignIn />} />
+        <Route path="auth/forgot-password" exact element={<ForgotPassword />} />
+        {/* <Route path="/Test" exact element={<Test />} /> */}
+        {/* <Route element={<PrivateRoutes />}>
+          <Route path="/dashboard" exact element={<Dashboard />} />
+          <Route path="/vehicles" exact element={<Vehicles />} />
+          <Route path="/stickers" exact element={<Stickers />} />
+          <Route path="/profile" exact element={<Profile />} />
+          
+        </Route> */}
+        {/* //TODO: Handle not found routes */}
+        {/* <Route path="*" element={`<div>404 Not found<div/>`} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/signUpNGO.js
@@ -0,0 +1,6 @@
+
+const SignUpNgo = () => {
+    return <>Sing Up Ngo</>
+}
+
+export default SignUpNgo
